8b9d5add25b06a35e60ffd2af4bae9a18fea7108
Benas <benas.sarachovas@gmail.com>
2024-09-25 17:58:59 +0300

    referencing modularized code to AI.js

--- AI.js
+++ AI.js
@@ -1,6 +1,10 @@
-import getBoard from getBoard
-import whoTurn from whoTurn
-import minimax from minimax
+import getBoard from './getBoard.js';
+import whoTurn from './whoTurn.js';
+import minimax from './minimax.js';
+import moveBestPiece from './moveBestPiece.js';
+import uploadImages from './uploadImages.js';
+import paintTiles from './paintTiles.js';
+import config from './config.js';
 
 function InitiateAI(){
     
@@ -15,20 +19,20 @@ function InitiateAI(){
     
     //if player turn Black then activate minimax
     if(currentPLayer == true){
-        let [value1, value2] = minimax(depth, checking_board, currentPLayer,alpha, beta)
+        let [value1, value2] = minimax(config.depth, checking_board, currentPLayer,alpha, beta)
         //console.log("Minimax Value : ", value2, " Best move : ", value1);
         moveBestPiece(value1.from, value1.to);
         //console.log("Total checked boards: ", checkedBoardCount)
-        checkedBoardCount = 0;
+        config.checkedBoardCount = 0;
         //console.log(pieceNameMemAI,"ALL MOVE PIECE NAMES")
         toggle++;
     }
 
     //upload images, repaint tiles, reset values
-    upload_Images()
+    uploadImages()
     paintTiles()
-    all_piece_moves = [];
-    black_pieces=[];
-    white_pieces = [];
+    config.all_piece_moves = [];
+    config.black_pieces=[];
+    config.white_pieces = [];
 }
 InitiateAI()
