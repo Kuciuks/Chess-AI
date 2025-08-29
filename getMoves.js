d4ca748baac90678a24f25272e30c7432c79ed9f
Benas <benas.sarachovas@gmail.com>
2025-08-29 11:47:29 +0100

    separated each move to its own method

--- getMoves.js
+++ getMoves.js
@@ -4,16 +4,6 @@ class moveDirections{
     getAvailableMoves(chess_board) {
         const tile_index = this.tile_index
 
-        const move_up_left = tile_index - 11
-        const move_down_left = tile_index + 9
-        const move_up_right = tile_index - 9
-        const move_down_right = tile_index + 11
-
-        const move_up = tile_index - 10
-        const move_down = tile_index + 10
-        const move_left = tile_index - 1
-        const move_right = tile_index + 1
-        
         const move_down = tile_index + 10
         const move_up = chess_board[tile_index - 10] == null ? tile_index - 10 : undefined
         const move_two_ahead =  this.special_move && chess_board[tile_index - 20] == null ? tile_index - 20 : undefined
@@ -23,92 +13,106 @@ class moveDirections{
 
         return valid_moves
     }
-}
 
 
 
+    moveUpLeft (){
+        //up left
+        for (let j = 0; j < 7; j++){
+            move -= 11 
+            if (chess_board[move] == null || chess_board[move]?.name !== "Border" && chess_board[move]?.color !== this.color && chess_board[move+11]?.color !== chess_board[move]?.color) { // changed from !== 'White' to !== chess_board[move]?.color so that it checks whether the next move up left is the same color as the previous move's color, this way l dont need to hardcode 'White' or 'Black'
+                valid_moves.push(move)
+            } else {
+                break
+            }
 
-//up left
-for (let j = 0; j < 7; j++){
-    move -= 11 
-    if (chess_board[move] == null || chess_board[move]?.name !== "Border" && chess_board[move]?.color !== this.color && chess_board[move+11]?.color !== chess_board[move]?.color) { // changed from !== 'White' to !== chess_board[move]?.color so that it checks whether the next move up left is the same color as the previous move's color, this way l dont need to hardcode 'White' or 'Black'
-        valid_moves.push(move)
-    } else {
-        break
+        }
     }
 
-}
-
-//down left
-for (let k = 0; k < 7; k++){
-    move += 9
-    if (chess_board[move] == null || chess_board[move]?.name !== "Border" && chess_board[move]?.color !== this.color && chess_board[move-9]?.color !== chess_board[move]?.color) {
-        valid_moves.push(move)
-    }
-    else {
-        break
-    }
-}
-
-
-//up right
-for (let l = 0; l < 7; l++){
-    move -= 9
-    if (chess_board[move] == null || chess_board[move]?.name !== "Border" && chess_board[move]?.color !== this.color && chess_board[move+9]?.color !== chess_board[move]?.color) {
-        valid_moves.push(move)
-    } else {
-        break
-    }
-}
-
-//down right
-for (let m = 0; m < 7; m++){
-    move += 11
-    if (chess_board[move] == null || chess_board[move]?.name !== "Border" && chess_board[move]?.color !== this.color && chess_board[move-11]?.color !== chess_board[move]?.color) {
-        valid_moves.push(move)
-    } else {
-        break
-    }
-}
-
-//up
-for (let j = 0; j < 7; j++){
-    move -= 10 
-    if (chess_board[move] == null || chess_board[move]?.name !== "Border" && chess_board[move]?.color !== this.color && chess_board[move+10]?.color !== chess_board[move]?.color) {
-        valid_moves.push(move)
-    } else {
-        break
+    moveDownLeft (){
+        //down left
+        for (let k = 0; k < 7; k++){
+            move += 9
+            if (chess_board[move] == null || chess_board[move]?.name !== "Border" && chess_board[move]?.color !== this.color && chess_board[move-9]?.color !== chess_board[move]?.color) {
+                valid_moves.push(move)
+            }
+            else {
+                break
+            }
+        }
     }
 
-}
-
-//down
-for (let k = 0; k < 7; k++){
-    move += 10 
-    if (chess_board[move] == null || chess_board[move]?.name !== "Border" && chess_board[move]?.color !== this.color && chess_board[move-10]?.color !== chess_board[move]?.color) {
-        valid_moves.push(move)
+    moveUpRight (){
+        //up right
+        for (let l = 0; l < 7; l++){
+            move -= 9
+            if (chess_board[move] == null || chess_board[move]?.name !== "Border" && chess_board[move]?.color !== this.color && chess_board[move+9]?.color !== chess_board[move]?.color) {
+                valid_moves.push(move)
+            } else {
+                break
+            }
+        }
     }
-    else {
-        break
-    }
-}
 
-//left
-for (let l = 0; l < 7; l++){
-    move -= 1 
-    if (chess_board[move] == null || chess_board[move]?.name !== "Border" && chess_board[move]?.color !== this.color && chess_board[move+1]?.color !== chess_board[move]?.color) {
-        valid_moves.push(move)
-    } else {
-        break
+    moveDownRight (){
+        //down right
+        for (let m = 0; m < 7; m++){
+            move += 11
+            if (chess_board[move] == null || chess_board[move]?.name !== "Border" && chess_board[move]?.color !== this.color && chess_board[move-11]?.color !== chess_board[move]?.color) {
+                valid_moves.push(move)
+            } else {
+                break
+            }
+        }
     }
-}
 
-//right
-for (let m = 0; m < 7; m++){
-    move += 1 
-    if (chess_board[move] == null || chess_board[move]?.name !== "Border" && chess_board[move]?.color !== this.color && chess_board[move-1]?.color !== chess_board[move]?.color) {
-        valid_moves.push(move)
-    } else {
-        break
+    moveUp (){
+        //up
+        for (let j = 0; j < 7; j++){
+            move -= 10 
+            if (chess_board[move] == null || chess_board[move]?.name !== "Border" && chess_board[move]?.color !== this.color && chess_board[move+10]?.color !== chess_board[move]?.color) {
+                valid_moves.push(move)
+            } else {
+                break
+            }
+
+        }
+    }
+
+    moveDown (){
+        //down
+        for (let k = 0; k < 7; k++){
+            move += 10 
+            if (chess_board[move] == null || chess_board[move]?.name !== "Border" && chess_board[move]?.color !== this.color && chess_board[move-10]?.color !== chess_board[move]?.color) {
+                valid_moves.push(move)
+            }
+            else {
+                break
+            }
+        }
+    }
+
+    moveLeft (){
+        //left
+        for (let l = 0; l < 7; l++){
+            move -= 1 
+            if (chess_board[move] == null || chess_board[move]?.name !== "Border" && chess_board[move]?.color !== this.color && chess_board[move+1]?.color !== chess_board[move]?.color) {
+                valid_moves.push(move)
+            } else {
+                break
+            }
+        }
+    }
+
+    moveRight (){
+        //right
+        for (let m = 0; m < 7; m++){
+            move += 1 
+            if (chess_board[move] == null || chess_board[move]?.name !== "Border" && chess_board[move]?.color !== this.color && chess_board[move-1]?.color !== chess_board[move]?.color) {
+                valid_moves.push(move)
+            } else {
+                break
+            }
+        }
     }
 }
