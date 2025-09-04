c653af28b32f2dfc5e8cf5497f016c60c9601dcb
Benas <benas.sarachovas@gmail.com>
2025-09-04 13:26:19 +0100

    refactored piece directional movement logic, made it more OOP. Added Rook, Bishop, Queen move acquiry

--- getMoves.js
+++ getMoves.js
@@ -1,118 +1,126 @@
 
-class moveDirections{
-    //method to get available moves for the pawn
-    getAvailableMoves(chess_board) {
-        const tile_index = this.tile_index
 
-        const move_down = tile_index + 10
-        const move_up = chess_board[tile_index - 10] == null ? tile_index - 10 : undefined
-        const move_two_ahead =  this.special_move && chess_board[tile_index - 20] == null ? tile_index - 20 : undefined
+function moveUpLeft (chess_board, move){ //move = piece tile index
+    const valid_moves = []
 
-
-        let valid_moves = this.checkMoveValidity(chess_board, [move_up_left, move_down_left, move_up_right, move_down_right])
-
-        return valid_moves
-    }
-
-
-
-    moveUpLeft (){
-        //up left
-        for (let j = 0; j < 7; j++){
-            move -= 11 
-            if (chess_board[move] == null || chess_board[move]?.name !== "Border" && chess_board[move]?.color !== this.color && chess_board[move+11]?.color !== chess_board[move]?.color) { // changed from !== 'White' to !== chess_board[move]?.color so that it checks whether the next move up left is the same color as the previous move's color, this way l dont need to hardcode 'White' or 'Black'
-                valid_moves.push(move)
-            } else {
-                break
-            }
-
+    //up left
+    for (let j = 0; j < 7; j++){
+        move -= 11 
+        if (chess_board[move] == null || chess_board[move]?.name !== "Border" && chess_board[move]?.color !== this.color && chess_board[move+11]?.color !== chess_board[move]?.color) { // changed from !== 'White' to !== chess_board[move]?.color so that it checks whether the next move up left is the same color as the previous move's color, this way l dont need to hardcode 'White' or 'Black'
+            valid_moves.push(move)
+        } else {
+            break
         }
     }
-
-    moveDownLeft (){
-        //down left
-        for (let k = 0; k < 7; k++){
-            move += 9
-            if (chess_board[move] == null || chess_board[move]?.name !== "Border" && chess_board[move]?.color !== this.color && chess_board[move-9]?.color !== chess_board[move]?.color) {
-                valid_moves.push(move)
-            }
-            else {
-                break
-            }
-        }
-    }
-
-    moveUpRight (){
-        //up right
-        for (let l = 0; l < 7; l++){
-            move -= 9
-            if (chess_board[move] == null || chess_board[move]?.name !== "Border" && chess_board[move]?.color !== this.color && chess_board[move+9]?.color !== chess_board[move]?.color) {
-                valid_moves.push(move)
-            } else {
-                break
-            }
-        }
-    }
-
-    moveDownRight (){
-        //down right
-        for (let m = 0; m < 7; m++){
-            move += 11
-            if (chess_board[move] == null || chess_board[move]?.name !== "Border" && chess_board[move]?.color !== this.color && chess_board[move-11]?.color !== chess_board[move]?.color) {
-                valid_moves.push(move)
-            } else {
-                break
-            }
-        }
-    }
-
-    moveUp (){
-        //up
-        for (let j = 0; j < 7; j++){
-            move -= 10 
-            if (chess_board[move] == null || chess_board[move]?.name !== "Border" && chess_board[move]?.color !== this.color && chess_board[move+10]?.color !== chess_board[move]?.color) {
-                valid_moves.push(move)
-            } else {
-                break
-            }
-
-        }
-    }
-
-    moveDown (){
-        //down
-        for (let k = 0; k < 7; k++){
-            move += 10 
-            if (chess_board[move] == null || chess_board[move]?.name !== "Border" && chess_board[move]?.color !== this.color && chess_board[move-10]?.color !== chess_board[move]?.color) {
-                valid_moves.push(move)
-            }
-            else {
-                break
-            }
-        }
-    }
-
-    moveLeft (){
-        //left
-        for (let l = 0; l < 7; l++){
-            move -= 1 
-            if (chess_board[move] == null || chess_board[move]?.name !== "Border" && chess_board[move]?.color !== this.color && chess_board[move+1]?.color !== chess_board[move]?.color) {
-                valid_moves.push(move)
-            } else {
-                break
-            }
-        }
-    }
-
-    moveRight (){
-        //right
-        for (let m = 0; m < 7; m++){
-            move += 1 
-            if (chess_board[move] == null || chess_board[move]?.name !== "Border" && chess_board[move]?.color !== this.color && chess_board[move-1]?.color !== chess_board[move]?.color) {
-                valid_moves.push(move)
-            } else {
-                break
-            }
-        }
-    }
+    return valid_moves
 }
+
+function moveDownLeft (chess_board, move){ //move = piece tile index
+    const valid_moves = []
+
+    //down left
+    for (let k = 0; k < 7; k++){
+        move += 9
+        if (chess_board[move] == null || chess_board[move]?.name !== "Border" && chess_board[move]?.color !== this.color && chess_board[move-9]?.color !== chess_board[move]?.color) {
+            valid_moves.push(move)
+        }
+        else {
+            break
+        }
+    }
+    return valid_moves
+}
+
+function moveUpRight (chess_board, move){ //move = piece tile index
+    const valid_moves = []
+
+    //up right
+    for (let l = 0; l < 7; l++){
+        move -= 9
+        if (chess_board[move] == null || chess_board[move]?.name !== "Border" && chess_board[move]?.color !== this.color && chess_board[move+9]?.color !== chess_board[move]?.color) {
+            valid_moves.push(move)
+        } else {
+            break
+        }
+    }
+    return valid_moves
+}
+
+function moveDownRight (chess_board, move){ //move = piece tile index
+    const valid_moves = []
+
+    //down right
+    for (let m = 0; m < 7; m++){
+        move += 11
+        if (chess_board[move] == null || chess_board[move]?.name !== "Border" && chess_board[move]?.color !== this.color && chess_board[move-11]?.color !== chess_board[move]?.color) {
+            valid_moves.push(move)
+        } else {
+            break
+        }
+    }
+    return valid_moves
+}
+
+function moveUp (chess_board, move){ //move = piece tile index
+    const valid_moves = []
+
+    //up
+    for (let j = 0; j < 7; j++){
+        move -= 10 
+        if (chess_board[move] == null || chess_board[move]?.name !== "Border" && chess_board[move]?.color !== this.color && chess_board[move+10]?.color !== chess_board[move]?.color) {
+            valid_moves.push(move)
+        } else {
+            break
+        }
+    }
+    return valid_moves
+}
+
+function moveDown (chess_board, move){ //move = piece tile index
+    const valid_moves = []
+
+    //down
+    for (let k = 0; k < 7; k++){
+        move += 10 
+        if (chess_board[move] == null || chess_board[move]?.name !== "Border" && chess_board[move]?.color !== this.color && chess_board[move-10]?.color !== chess_board[move]?.color) {
+            valid_moves.push(move)
+        }
+        else {
+            break
+        }
+    }
+    return valid_moves
+}
+
+function moveLeft (chess_board, move){ //move = piece tile index
+    const valid_moves = []
+
+    //left
+    for (let l = 0; l < 7; l++){
+        move -= 1 
+        if (chess_board[move] == null || chess_board[move]?.name !== "Border" && chess_board[move]?.color !== this.color && chess_board[move+1]?.color !== chess_board[move]?.color) {
+            valid_moves.push(move)
+        } else {
+            break
+        }
+    }
+    return valid_moves
+}
+
+function moveRight (chess_board, move){ //move = piece tile index
+    const valid_moves = []
+
+    //right
+    for (let m = 0; m < 7; m++){
+        move += 1 
+        if (chess_board[move] == null || chess_board[move]?.name !== "Border" && chess_board[move]?.color !== this.color && chess_board[move-1]?.color !== chess_board[move]?.color) {
+            valid_moves.push(move)
+        } else {
+            break
+        }
+    }
+    return valid_moves
+}
+
+
+export default {moveUpLeft, moveDownLeft, moveUpRight, moveDownRight, moveUp, moveDown, moveLeft, moveRight}
